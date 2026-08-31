7d45981ae0a49b888d6dad4cc8159b4a7851bbab
dede-08 <benandrealanyaguevara@gmail.com>
2026-08-31 18:33:37 -0500

    servicios cargados

---
+++ property-service/src/test/java/com/stayhub/app/property_service/PropertyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayhub.app.property_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
